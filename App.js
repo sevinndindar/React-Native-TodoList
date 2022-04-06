6469dd9147cc077988807ff30355c19c6c9a530a
Aksamci <Aksamci@DESKTOP-M3VTEMO>
2022-04-06 14:46:12 +0300

    react-native

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View ,FlatList} from 'react-native';
+import { useState, useEffect } from "react"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import Header from "./Components/Header";
+import Input from "./Components/Input";
+import Todo from "./Components/Todo";
 export default function App() {
-  return (
+  const[todoList,setTodoList]=useState([]);
+  const addTodo=(newTodo) => {
+    const tmpTodoList=[...todoList];
+    tmpTodoList.push(newTodo);
+    setTodoList(tmpTodoList);
+  };
+  const deleteTodo=(indexTodo) => {
+    const tmpTodoList=[...todoList];
+    tmpTodoList.splice(indexTodo, 1);
+    setTodoList(tmpTodoList);
+  };
+  const getTodos=() => {
+    AsyncStorage.getItem("todoList").then((jsonTodoList)=>{
+      const todos = JSON.parse(jsonTodoList || "[]");
+      setTodoList(todos);
+    });
+  };
+  const saveTodo =() => {
+    AsyncStorage.setItem("todoList",JSON.stringify(todoList))
+      .then(() =>{
+        console.log("Save OK!");
+      })
+      .catch((err) => {
+        console.log("ERROR!!" + err.Message);
+      });
+  };
+  const updateTodoDone=(indexTodo)=>{
+    const tmpTodoList=[...todoList];
+    tmpTodoList[indexTodo].done=!tmpTodoList[indexTodo].done;
+    setTodoList(tmpTodoList);
+  };
+  useEffect(() =>{
+    getTodos();
+  },[]);
+  useEffect(() =>{
+    saveTodo();
+  });
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.headerContainer}>
+        <Header></Header>
+      </View>
+      <View>
+        <Input addTodo={addTodo}></Input>
+      </View>
+      <View style={styles.todoListContainer}>
+        <FlatList
+          data={todoList}
+          renderItem={({ item,index}) =>(
+            <Todo
+            nameTodo={item.name}
+            todoDone={item.done}
+            indexTodo={index}
+            deleteTodo={deleteTodo}
+            updateTodoDone={updateTodoDone}
+            ></Todo>
+          )}
+          keyExtractor={(item,index) => index.toString()}
+          ></FlatList>
+      </View>
+      <StatusBar hidden={true} style="auto"/>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+    },
+    headerContainer: {
+      flex: 1,
+      backgroundColor: "#0074D9",
+    },
+    inputContainer: {
+      flex: 2,
+    },
+    todolistContainer: {
+      flex: 5,
+      backgroundColor: "#AAAAAA",
+    }
+  });
---
+++ Components/Header.js
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default Header = () => {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>First react Native Project</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: { fontSize: 30, color: "white" },
+});
